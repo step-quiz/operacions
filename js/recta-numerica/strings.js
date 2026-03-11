41468386251bb2d54ac2665479d161a2dda85633
step-quiz <darso@xtec.cat>
2026-03-11 08:50:13 +0100

    Update strings.js

--- js/recta-numerica/strings.js
+++ js/recta-numerica/strings.js
@@ -94,9 +94,9 @@ window.Strings = (() => {
         correct_q3_zero:    (y)    => `✓ Correcte! Cap punt del gràfic té una temperatura de ${tempLabel(y)}.`,
         correct_q3_some:    (n, y) => `✓ Correcte! Hi ha ${n === 1 ? '1 dia' : `${n} dies`} amb temperatura de ${tempLabel(y)}.`,
         correct_q4:         '✓ Correcte! Has llegit bé el gràfic.',
-        wrong_q1q2:         'Busca bé el quin dia toca, a l\'eix horitzontal, i després llegeix el valor a l\'eix vertical.',
-        wrong_q3:           'Torna a comptar els punts que tenen exactament aquesta temperatura.',
-        wrong_q4:           'Llegeix atentament el gràfic abans de triar.',
+        wrong_q1q2:         'Busca el dia a l\'eix horitzontal, i llegeix el valor a l\'eix vertical.',
+        wrong_q3:           'Torna a comptar els punts.',
+        wrong_q4:           'Llegeix atentament el gràfic.',
     };
 
     function capitalize(s) {
