3edebb42bc6b32723957641a8713b6763f840cd9
step-quiz <darso@xtec.cat>
2026-05-04 09:46:31 +0200

    Update config.js

--- PAU/config.js
+++ PAU/config.js
@@ -24,5 +24,6 @@ const OPTATIUS = new Set([
 const VIDEOS = {
   // 'alg-23j-q2': 'XXXXXXXXXXX',
   // 'ana-24j-q1': 'YYYYYYYYYYY',
-   'ana-24j-q1':'bW5Cz1lC4TA'
+   'ana-24j-q1':'bW5Cz1lC4TA',
+   'ana-24s-q1':'lB4Q0zkDgiA'
 };
